92b20ad6ae2265909936029ad5e7c2aaf70fbd69
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-04-09 21:05:12 -0500

    feat: :construction: Comienzo de función para mostrar diferentes elementos de páginación

--- js/main.js
+++ js/main.js
@@ -9,8 +9,20 @@ async function getCharacters() {
     const response = await fetch("https://dattebayo-api.onrender.com/characters");
     const json = await response.json();
     characters = json.characters;
+    showPage(currentPage);
   } catch (error) {
     console.error("Error al obtener personajes:", error);
   }
 }
 
+function showPage(pageNumber) {
+  if (pageNumber < 1 || pageNumber > 5) return;
+
+  currentPage = pageNumber; 
+
+  const startIndex = (pageNumber - 1) * charactersPerPage;
+  const endIndex = startIndex + charactersPerPage;
+  const currentCharacters = characters.slice(startIndex, endIndex);
+
+  cardsContainer.innerHTML = "";
+}
